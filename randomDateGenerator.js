a3407eaef6341dd01977c5aece4cb9c3bf1b668d
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2022-04-16 04:31:18 -0500

    changed website

--- randomDateGenerator.js
+++
@@ -1,5 +0,0 @@
-// function randomDate(start, end) {
-//     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-// }
-
-// return randomDate(new Date(2012, 0, 1), new Date())
